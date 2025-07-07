6d8ddefd018173e5e78f122ddd17e61ad81bfa6a
Fernando Herrera <fernando.herrera85@gmail.com>
2025-07-07 17:03:36 -0400

    feat: update HeroesLayout with navigation links and lazy load SearchPage

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,3 +1,4 @@
+import { lazy } from 'react';
 import { createBrowserRouter } from 'react-router';
 
 import { AdminLayout } from '@/admin/layouts/AdminLayout';
@@ -5,7 +6,10 @@ import { AdminPage } from '@/admin/pages/AdminPage';
 import { HeroesLayout } from '@/heroes/layouts/HeroesLayout';
 import { HeroPage } from '@/heroes/pages/hero/HeroPage';
 import { HomePage } from '@/heroes/pages/home/HomePage';
-import { SearchPage } from '@/heroes/pages/search/SearchPage';
+
+// import { SearchPage } from '@/heroes/pages/search/SearchPage';
+
+const SearchPage = lazy(() => import('@/heroes/pages/search/SearchPage'));
 
 export const appRouter = createBrowserRouter([
   {
