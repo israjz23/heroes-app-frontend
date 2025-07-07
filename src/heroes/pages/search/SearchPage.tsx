2f630085ec54df87983c71dab755e87fb3b67a15
Fernando Herrera <fernando.herrera85@gmail.com>
2025-07-07 17:49:29 -0400

    feat: enhance SearchPage with CustomJumbotron and HeroStats components
    - Added CustomJumbotron for improved hero search experience with title and description.
    - Integrated HeroStats component to display various hero statistics.
    - Created CustomJumbotron component for reusable jumbotron UI.
    - Introduced HeroStatCard component to encapsulate individual hero statistics.
    - Implemented Badge component for displaying hero categories.
    - Developed Card component structure for consistent UI layout.
    - Added input, progress, and tabs UI components for enhanced user interaction.
    - Included placeholder SVG for default image representation.

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,5 +1,18 @@
+import { CustomJumbotron } from '@/components/custom/CustomJumbotron';
+import { HeroStats } from '@/heroes/components/HeroStats';
+
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <>
+      <CustomJumbotron
+        title="Búsqueda de SuperHéroes"
+        description="Descubre, explora y administra super héroes y villanos"
+      />
+
+      {/* Stats Dashboard */}
+      <HeroStats />
+    </>
+  );
 };
 
 export default SearchPage;
