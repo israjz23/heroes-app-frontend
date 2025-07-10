2d1b4ecae394b73862c17a7110e32e520ad3b0e6
Fernando Herrera <fernando.herrera85@gmail.com>
2025-07-10 14:34:35 -0400

    feat: implement search functionality in SearchPage and add searchHeroesAction for API integration

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -2,8 +2,22 @@ import { CustomJumbotron } from '@/components/custom/CustomJumbotron';
 import { HeroStats } from '@/heroes/components/HeroStats';
 import { SearchControls } from './ui/SearchControls';
 import { CustomBreadcrumbs } from '@/components/custom/CustomBreadcrumbs';
+import { HeroGrid } from '@/heroes/components/HeroGrid';
+import { useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router';
+import { searchHeroesAction } from '@/heroes/actions/search-heros.action';
 
 export const SearchPage = () => {
+  const [searchParams] = useSearchParams();
+
+  const name = searchParams.get('name') ?? undefined;
+
+  const { data: heroes = [] } = useQuery({
+    queryKey: ['search', { name }],
+    queryFn: () => searchHeroesAction({ name }),
+    staleTime: 1000 * 60 * 5, // 5 minutos
+  });
+
   return (
     <>
       <CustomJumbotron
@@ -25,6 +39,10 @@ export const SearchPage = () => {
 
       {/* Filter and search */}
       <SearchControls />
+
+      {/*  */}
+
+      <HeroGrid heroes={heroes} />
     </>
   );
 };
