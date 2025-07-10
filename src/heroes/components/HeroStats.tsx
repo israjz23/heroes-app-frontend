4eeb834031f1570ad5657bf2d257354dff611f4d
Fernando Herrera <fernando.herrera85@gmail.com>
2025-07-10 13:10:19 -0400

    feat: integrate favorite hero functionality in HeroGridCard, HeroStats, and HomePage components

--- src/heroes/components/HeroStats.tsx
+++ src/heroes/components/HeroStats.tsx
@@ -1,17 +1,28 @@
+import { use } from 'react';
 import { Users, Heart, Zap } from 'lucide-react';
 import { HeroStatCard } from './HeroStatCard';
 
 import { Badge } from '@/components/ui/badge';
 import { useHeroSummary } from '../hooks/useHeroSummary';
+import { FavoriteHeroContext } from '../context/FavoriteHeroContext';
 
 export const HeroStats = () => {
   const { data: summary } = useHeroSummary();
+  const { favoriteCount } = use(FavoriteHeroContext);
+
+  // const percentageFavorite = useMemo(() => {
+  //   const percentage = favoriteCount / summary?.totalHeroes
+  // },[favoriteCount, summary])
 
   // const { data: summary } = useQuery({
   //   queryKey: ['summary-information'],
   //   queryFn: getSummaryAction,
   //   staleTime: 1000 * 60 * 5, // 5 minutos
   // });
+
+  if (!summary) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-8">
@@ -35,8 +46,10 @@ export const HeroStats = () => {
         icon={<Heart className="h-4 w-4 text-muted-foreground" />}
       >
         {/* TODO: tenemos que calcular este valor */}
-        <div className="text-2xl font-bold text-red-600">3</div>
-        <p className="text-xs text-muted-foreground">18.8% of total</p>
+        <div className="text-2xl font-bold text-red-600">{favoriteCount}</div>
+        <p className="text-xs text-muted-foreground">
+          {((favoriteCount / summary.totalHeroes) * 100).toFixed(2)}% of total
+        </p>
       </HeroStatCard>
 
       <HeroStatCard
--- src/heroes/pages/home/HomePage.tsx
+++ src/heroes/pages/home/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { use, useMemo } from 'react';
 import { useSearchParams } from 'react-router';
 
 import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
@@ -10,9 +10,11 @@ import { CustomBreadcrumbs } from '@/components/custom/CustomBreadcrumbs';
 
 import { useHeroSummary } from '@/heroes/hooks/useHeroSummary';
 import { usePaginatedHero } from '@/heroes/hooks/usePaginatedHero';
+import { FavoriteHeroContext } from '@/heroes/context/FavoriteHeroContext';
 
 export const HomePage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { favoriteCount, favorites } = use(FavoriteHeroContext);
 
   const activeTab = searchParams.get('tab') ?? 'all';
   const page = searchParams.get('page') ?? '1';
@@ -67,7 +69,7 @@ export const HomePage = () => {
                 })
               }
             >
-              Favorites (3)
+              Favorites ({favoriteCount})
             </TabsTrigger>
             <TabsTrigger
               value="heroes"
@@ -103,8 +105,7 @@ export const HomePage = () => {
           </TabsContent>
           <TabsContent value="favorites">
             {/* Mostrar todos los personajes favoritos */}
-            <h1>Favoritos!!!</h1>
-            {/* <HeroGrid heroes={heroesResponse?.heroes ?? []} /> */}
+            <HeroGrid heroes={favorites} />
           </TabsContent>
           <TabsContent value="heroes">
             {/* Mostrar todos los héroes */}
@@ -119,8 +120,9 @@ export const HomePage = () => {
         </Tabs>
 
         {/* Pagination */}
-
-        <CustomPagination totalPages={heroesResponse?.pages ?? 1} />
+        {selectedTab !== 'favorites' && (
+          <CustomPagination totalPages={heroesResponse?.pages ?? 1} />
+        )}
       </>
     </>
   );
