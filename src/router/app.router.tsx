3d2e3388909e98b1cf2c2d3fdafa6060eca3fe29
israel jaime zamorategui <israel.jaime34@my.unitec.edu.mx>
2026-01-04 02:18:06 -0600

    initial commit

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,19 +1,20 @@
-import { lazy } from 'react';
-import { createBrowserRouter, Navigate } from 'react-router';
+import { lazy } from "react";
+import { createHashRouter, Navigate } from "react-router";
 
-import { AdminLayout } from '@/admin/layouts/AdminLayout';
-import { AdminPage } from '@/admin/pages/AdminPage';
-import { HeroesLayout } from '@/heroes/layouts/HeroesLayout';
-import { HeroPage } from '@/heroes/pages/hero/HeroPage';
-import { HomePage } from '@/heroes/pages/home/HomePage';
+import { AdminLayout } from "@/admin/layouts/AdminLayout";
+import { AdminPage } from "@/admin/pages/AdminPage";
+import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
+import { HeroPage } from "@/heroes/pages/hero/HeroPage";
+import { HomePage } from "@/heroes/pages/home/HomePage";
 
 // import { SearchPage } from '@/heroes/pages/search/SearchPage';
 
-const SearchPage = lazy(() => import('@/heroes/pages/search/SearchPage'));
+const SearchPage = lazy(() => import("@/heroes/pages/search/SearchPage"));
 
-export const appRouter = createBrowserRouter([
+// export const appRouter = createBrowserRouter([
+export const appRouter = createHashRouter([
   {
-    path: '/',
+    path: "/",
     element: <HeroesLayout />,
     children: [
       {
@@ -21,15 +22,15 @@ export const appRouter = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: 'heroes/:idSlug',
+        path: "heroes/:idSlug",
         element: <HeroPage />,
       },
       {
-        path: 'search',
+        path: "search",
         element: <SearchPage />,
       },
       {
-        path: '*',
+        path: "*",
         // element: <h1>404</h1>,
         element: <Navigate to="/" />,
       },
@@ -37,7 +38,7 @@ export const appRouter = createBrowserRouter([
   },
 
   {
-    path: '/admin',
+    path: "/admin",
     element: <AdminLayout />,
     children: [
       {
